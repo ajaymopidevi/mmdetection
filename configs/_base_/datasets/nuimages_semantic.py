2a40185a9bfe5a6d924f4984fd901fc945ce8ca1
ajaymopidevi <ajaymopidevi@gmail.com>
2024-06-17 23:10:53 -0600

    Support nuimages dataset

---
+++ configs/_base_/datasets/nuimages_semantic.py
@@ -0,0 +1,150 @@
+
+dataset_type = 'CocoDataset'
+data_root = 'data/nuimages/'
+class_names = [
+    'car', 'truck', 'trailer', 'bus', 'construction_vehicle', 'bicycle',
+    'motorcycle', 'pedestrian', 'traffic_cone', 'barrier'
+]
+
+# Example to use different file client
+# Method 1: simply set the data root and let the file I/O module
+# automatically infer from prefix (not support LMDB and Memcache yet)
+
+# data_root = 's3://openmmlab/datasets/detection3d/nuimages/'
+
+# Method 2: Use backend_args, file_client_args in versions before 1.1.0
+# backend_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection3d/',
+#          'data/': 's3://openmmlab/datasets/detection3d/'
+#      }))
+backend_args = None
+
+# train_cfg = dict(max_epochs=12, type='EpochBasedTrainLoop', val_interval=1)
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=False, with_mask=False, with_seg=True),
+    dict(
+        type='Resize',
+        scale=(1280, 720),
+        keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs'),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(1280, 720), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=False, with_mask=False, with_seg=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='train/annotations/nuimages_v1.0-train.json',
+        data_prefix=dict(img='./', seg='train/annotations/semantic_masks/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args))
+
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='val/annotations/nuimages_v1.0-val.json',
+        data_prefix=dict(img='./', seg='val/annotations/semantic_masks/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='test/annotations/nuimages_v1.0-test.json',
+        data_prefix=dict(img='./', seg='test/annotations/semantic_masks/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+
+val_evaluator = dict(type='SemSegMetric', iou_metrics=['mIoU'])
+test_evaluator = val_evaluator
+
+# val_evaluator = dict(
+#     type='CocoMetric',
+#     ann_file=data_root + 'val/annotations/nuimages_v1.0-val.json',
+#     metric=['bbox', 'segm'],
+#     format_only=False,
+#     backend_args=backend_args)
+
+# test_evaluator = dict(
+#     type='CocoMetric',
+#     ann_file=data_root + 'test/annotations/nuimages_v1.0-test.json',
+#     metric=['bbox', 'segm'],
+#     format_only=False,
+#     backend_args=backend_args)
+
+# data = dict(
+#     samples_per_gpu=2,
+#     workers_per_gpu=2,
+#     train=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'annotations/nuimages_v1.0-train.json',
+#         img_prefix=data_root,
+#         classes=class_names,
+#         pipeline=train_pipeline),
+#     val=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'annotations/nuimages_v1.0-val.json',
+#         img_prefix=data_root,
+#         classes=class_names,
+#         pipeline=test_pipeline),
+#     test=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'annotations/nuimages_v1.0-val.json',
+#         img_prefix=data_root,
+#         classes=class_names,
+#         pipeline=test_pipeline))
+# evaluation = dict(metric=['bbox', 'segm'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
